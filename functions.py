18cfa8b6525b933c4c31d4f18b7a28e2f1cd34b5
SaverioPCapra <107687546+SaverioPCapra@users.noreply.github.com>
2025-03-27 20:18:35 +0100

    implemented randomization_inference

--- functions.py
+++ functions.py
@@ -28,7 +28,7 @@ def create_regression_table(data, outcome, predictors_list, control_sets, signif
         X = sm.add_constant(X)  # Add a constant term
         y = data[outcome]
 
-        model = sm.OLS(y, X).fit()
+        model = sm.OLS(y, X).fit(cov_type= "HC3")
 
         for predictor in predictors_list + control_vars + ['const']:
             if predictor not in results:
@@ -80,3 +80,47 @@ def create_regression_table(data, outcome, predictors_list, control_sets, signif
     table_data.append(["R-squared"] + [r_squared for _ in range(len(control_sets))])
     table = tabulate(table_data, headers=headers, tablefmt="pipe")
     return table
+
+def randomization_inference(df, treatment_col, y_col, percentile_input = 5, n_permutations = 1000):
+    # Number of permutations that I will run to carry out the randomization inference
+    num_permutations = n_permutations
+
+    df_notreatment = df.copy()
+    df_notreatment = df_notreatment.drop([treatment_col], axis = 1)
+
+    n_subjects = df[treatment_col].sum()
+
+    reg_coefficients = []
+
+    for i in range(num_permutations):
+        # Creates a permuted version of the DataFrame by randomly shuffling the rows without replacement
+        permuted_df = df_notreatment.sample(frac=1, replace=False)
+
+        # I assign to this variable the number of units to which I will have to assign the treatment in the MC simulation 
+        # n_treatment = number of subjects that received treatment
+        n_treatment = n_subjects
+
+        # I assign to the first "n_treatment" rows of the permuted dataframe the treatment
+        treatment_df = permuted_df.copy().iloc[:n_treatment]
+        treatment_df["treated"] =  1
+
+        # I leave the remaining entries untreated
+        no_treatment_df = permuted_df.copy().iloc[n_treatment:]
+        no_treatment_df["treated"] = 0
+
+        # I concatenate the treatment and non-treatment dataframe
+        # I do this, so that then I can run my function to estimate the coefficient of the treatment dummy
+        complete_df = pd.concat([treatment_df, no_treatment_df], axis = 0)
+
+        # I estimate the treatment coefficient
+        reg = sm.OLS(complete_df[y_col], sm.add_constant(complete_df.drop([y_col], axis = 1))).fit(cov_type="HC3")
+        treatment_coefficient = reg.params['treated']
+
+        reg_coefficients.append(treatment_coefficient)
+
+    percentile_lower_regcoeff = np.percentile(reg_coefficients,percentile_input)
+    percentile_higher_regcoeff = np.percentile(reg_coefficients,100-percentile_input)
+
+    return (percentile_lower_regcoeff, percentile_higher_regcoeff, reg_coefficients)
+
+        
